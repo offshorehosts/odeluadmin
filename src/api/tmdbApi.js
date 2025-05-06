a44edb28906835b33b3f802d35f2874c9b6e0340
offshorehosts <odelu0@proton.me>
2025-05-06 15:30:53 +0530

    Update tmdbApi.js

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const TMDB_API_KEY = process.env.TMDB_API_KEY; 
+const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY; 
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 
